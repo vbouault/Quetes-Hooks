55b9346326ee403013940b9d2edb2ba6cdbc0f8a
Vianney Bouault <vbouault@hotmail.fr>
2020-04-27 12:35:05 +0200

    add input initial value

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  // Déclare une nouvelle variable d'état, qu’on va appeler « count »
+  // Déclare une fonction setCount qui permet de modifier la valeur de count
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <input type="number" onChange={(event) => setCount(parseInt(event.target.value,10))}/>
+      <p>Le compteur est  à : {count} </p>
+      <button onClick={() => setCount(count + 1)}>
+        +1
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        -1
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
